ec52d39f80dfc9cec8080a48cd2b971dc9f2b75d
Harsh Raj Singh <135263337+Harshrajput73@users.noreply.github.com>
2024-01-19 00:54:41 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,14 +6,14 @@ function Footer() {
     <div className="footer flex justify-center  bg-cyan-700 border-2  p-4 fixed w-full bottom-0 ">
       <div className="flex">
         <div className="flex relative -top-[3px]">
-          <a href="https://github.com/Saroj-Kumarr">
+          <a href="https://github.com/Harshrajput73">
             <FaGithubSquare className="m-1 text-2xl rounded-xl" />
           </a>
           <FaLinkedin className="m-1 text-sky-500 text-2xl rounded-xl" />
           <FaInstagramSquare className="m-1 text-pink-600 text-2xl rounded-xl" />
         </div>
         <div className="font-bold">
-          | Developed by ❤️ Saroj <span className="text-white">Kumar</span>{" "}
+          | Developed by ❤️ Harsh Raj <span className="text-white">Singh</span>{" "}
         </div>
       </div>
     </div>
