00f0fdca3ea2cd3a2b74dd7345454b8dd2526146
Harsh Raj Singh <135263337+Harshrajput73@users.noreply.github.com>
2024-01-19 00:53:15 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="header  flex bg-cyan-700 border-2 p-4 justify-between fixed w-full top-0 z-20 h-20 ">
       <h1 className="font-bold  text-3xl relative left-8 max-[600]:relative max-[600]:left-10 ">
-        Saroj <span className="text-white">Kumar</span>
+        Harsh Raj <span className="text-white">Singh</span>
       </h1>
       <div className=" text-white font-bold absolute right-2 top-4  max-[600px]:mr-3 max-[600px]:mt-1 max-[600px]:hidden">
         <button className="border-2 p-[7px] relative -left-4 border-black bg-white text-black font-bold">
